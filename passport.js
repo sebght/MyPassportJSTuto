cc83ad79695a28ea8ce5ff98d9c604d538d412b7
Sebastien Gahat <seb.gahat@gmail.com>
2018-11-08 01:32:34 +0100

    mise en forme

--- passport.js
+++ passport.js
@@ -1,42 +1,3 @@
-/*//passport.js
-const passport = require('passport');
-const LocalStrategy = require('passport-local').Strategy;
-const passportJWT = require("passport-jwt");
-const JWTStrategy   = passportJWT.Strategy;
-const ExtractJWT = passportJWT.ExtractJwt;
-
-passport.use(new LocalStrategy({
-        usernameField: 'email',
-        passwordField: 'password'
-    },
-    function (email, password, cb) {
-        // cette fonction vérifie la validité des identifiants.
-        return UserModel.findOne({email, password})
-            .then(user => {
-                if (!user) {
-                    return cb(null, false, {message: 'Incorrect email or password.'});
-                }
-                return cb(null, user, {message: 'Logged In Successfully'});
-            })
-            .catch(err => cb(err));
-    }
-));
-passport.use(new JWTStrategy({
-        jwtFromRequest: ExtractJWT.fromAuthHeaderAsBearerToken(),
-        secretOrKey   : 'your-256-bit-secret'
-    },
-    function (jwtPayload, cb) {
-        //recherche le user dans la base de données souhaitée
-        return UserModel.findOneById(jwtPayload.id)
-            .then(user => {
-                return cb(null, user);
-            })
-            .catch(err => {
-                return cb(err);
-            });
-    }
-));*/
-
 const passport    = require('passport');
 const passportJWT = require("passport-jwt");
 
@@ -62,30 +23,15 @@ passport.use(new LocalStrategy({
     function (email, password, cb) {
       console.log(email, password);
 
-        //Assume there is a DB module providing a global UserModel
+        // cette fonction vérifie la validité des identifiants.
         if (email!=user.email || password!=user.password){
-            console.log("login pas ok")
+            console.log("On a un problème chef ! (login incorrect)")
             return cb(null, false, {message: 'Incorrect email or password.'});
         }
-        console.log("login ok")
+        console.log("Le login est réussi, c'est beau")
         return cb(null, user, {
             message: 'Logged In Successfully'
         })
-        /*
-        // cette fonction vérifie la validité des identifiants.
-        return UserModel.findOne({email, password})
-            .then(user => {
-                if (!user) {
-                    return cb(null, false, {message: 'Incorrect email or password.'});
-                }
-
-                return cb(null, user, {
-                    message: 'Logged In Successfully'
-                });
-            })
-            .catch(err => {
-                return cb(err);
-            });*/
     }
 ));
 
